fe9aec5eee507a3e8b8e63d9a8308f8158ae13ad
Anssi Aitta <anssi.aitta@granlund.fi>
2025-12-17 12:45:01 +0200

    port changes: removed https

--- ClientApp/src/setupProxy.js
+++ ClientApp/src/setupProxy.js
@@ -7,7 +7,7 @@ const target = env.ASPNETCORE_HTTPS_PORT
   ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}`
   : env.ASPNETCORE_URLS
     ? env.ASPNETCORE_URLS.split(';')[0]
-    : 'https://localhost:7124';
+    : 'http://localhost:5267';
 
 // Proxy any API calls under /api to the backend
 const context = [
